bf6dba75316b7c3effc3130c0d76a8fb1b94e9a1
GauriVJ <gvjadhav039@gmail.com>
2023-10-21 13:44:18 +0530

    QuizApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QuizApp';
+  score = 0;
+  showscore= false; //state of answer
+  isstart = false;
+  selectedAnswers: string[] = [];
+  Againscore = true;
+  AttemptAgain = true;
+  iterate = false;
+  //dispscore = false;
+
+  // constructor(private cdr: ChangeDetectorRef) {
+  //   // Your other constructor code here
+  // }
+  // [what was this??]
+
+  questions: any[] = [
+    {
+      questionText: 'What does HTTP stands for?',
+      answers: ['Hypertext Transfer Protocol', 'Hyper Transfer Text Protocol', 'High Transfer Text Protocol', 'None of the above'],
+      correctAnswer: 'Hypertext Transfer Protocol'
+
+    },
+    {
+      questionText: 'CSS stands for',
+      answers: ['Cascading Style Sheets', 'CAsting style sheets', 'Casting and Scripting Sheets', 'Nonw of the above'],
+      correctAnswer: 'Cascading Style Sheets'
+    },
+    {
+      questionText: 'CSS stands for',
+      answers: ['Cascading Style Sheets', 'CAsting style sheets', 'Casting and Scripting Sheets', 'Nonw of the above'],
+      correctAnswer: 'Cascading Style Sheets'
+    }
+
+    //yet to add questions
+  ];
+
+  Start() {
+    this.isstart = true;
+    this.iterate = true;
+  }
+
+  check(question: any, selectedAnswer: string) {
+    console.log('selectedAnswer', selectedAnswer)
+    console.log('question', question)
+    if (selectedAnswer === question.correctAnswer) {
+      this.score = this.score + 1;
+    }
+  }
+
+  submitAnswers() {
+   
+    this.showscore = true;
+    this.Againscore = false;
+    this.AttemptAgain = false;
+
+  }
+
+
 }
+
+
